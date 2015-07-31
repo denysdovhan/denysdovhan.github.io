e4acfb4d8ca9772f6e0c0f3a0943d2f55a721f0c
Denys Dovhan <email@denysdovhan.com>
2015-07-31 21:58:40 +0300

    Add markdown renderer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
-var gulp = require('gulp');
+'use strict';
+
+var gulp        = require('gulp'),
+    jade        = require('gulp-jade'),
+    marked      = require('gulp-marked'),
+    stylus      = require('gulp-stylus'),
+    frontMatter = require('gulp-front-matter'),
+    through     = require('through2');
+
+
+gulp.task('posts', function () {
+  return gulp.src('posts/*.md')
+      .pipe(frontMatter({
+        property: 'post',
+        remove: true
+      }))
+      .pipe(marked())
+      .pipe(gulp.dest('dist'));
+});
