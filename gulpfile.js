fbc1f422a257bd63a8e334213d03ac9b825f9c49
Denys Dovhan <email@denysdovhan.com>
2015-07-31 23:42:21 +0300

    Add collect function.

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,46 @@ var gulp        = require('gulp'),
     marked      = require('gulp-marked'),
     stylus      = require('gulp-stylus'),
     frontMatter = require('gulp-front-matter'),
-    through     = require('through2');
+    through     = require('through2'),
+    path        = require('path');
+
+
+var posts = [];
+function collect() {
+  return through.obj(function (file, enc, cb) {
+    file.data.url = 'posts/' + path.basename(file.path, '.md');
+
+    posts.push({
+      data: file.data,
+      content: file.contents.toString()
+    });
+
+    this.push(file);
+    cb();
+  },
+  function (cb) {
+    posts.sort(function (a, b) {
+      return b.date - a.date;
+    });
+    cb();
+  });
+}
 
 
 gulp.task('posts', function () {
   return gulp.src('posts/*.md')
       .pipe(frontMatter({
-        property: 'post',
+        property: 'data',
         remove: true
       }))
       .pipe(marked())
+      .pipe(collect())
       .pipe(gulp.dest('dist'));
 });
 
 
 gulp.task('styles', function () {
-  return gulp.src(['styles/*','!styles/_*'])
+  return gulp.src(['styles/*','!jstyles/_*'])
       .pipe(stylus())
       .pipe(gulp.dest('dist/styles'));
 });
