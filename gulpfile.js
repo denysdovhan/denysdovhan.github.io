96ad27d005dfd32764e0d36dc0ae144b0622df02
Denys Dovhan <email@denysdovhan.com>
2015-08-01 19:38:08 +0300

    Gulp: Add post rendering.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@
 
 var gulp        = require('gulp'),
     jade        = require('gulp-jade'),
+    data        = require('gulp-data'),
     marked      = require('gulp-marked'),
+    rename      = require('gulp-rename'),
     stylus      = require('gulp-stylus'),
     frontMatter = require('gulp-front-matter'),
     through     = require('through2'),
+    each        = require('each-done'),
     path        = require('path');
 
 
 var posts = [];
 function collect() {
   return through.obj(function (file, enc, cb) {
-    file.data.url = 'posts/' + path.basename(file.path, '.md');
+    file.data.url = path.basename(file.path, path.extname(file.path));
 
     posts.push({
       data: file.data,
@@ -30,19 +33,27 @@ function collect() {
   });
 }
 
-
-gulp.task('posts', function () {
+// Collect all posts
+gulp.task('collect', function () {
+  posts = [];
   return gulp.src('posts/*.md')
-      .pipe(frontMatter({
-        property: 'data',
-        remove: true
-      }))
+      .pipe(frontMatter({ property: 'data', remove: true }))
       .pipe(marked())
-      .pipe(collect())
-      .pipe(gulp.dest('dist'));
+      .pipe(collect());
 });
 
+// Render all posts
+gulp.task('render', ['collect'], function (done) {
+  each(posts, function (post) {
+    return gulp.src('layout/post.jade')
+      .pipe(data(post))
+      .pipe(jade({ pretty: true }))
+      .pipe(rename({ dirname: post.data.url, basename: 'index' }))
+      .pipe(gulp.dest('dist'));
+  }, done);
+});
 
+// Render styles
 gulp.task('styles', function () {
   return gulp.src(['styles/*','!jstyles/_*'])
       .pipe(stylus())
