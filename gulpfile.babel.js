a761ef44083b0e2b8a7e4cad5ec2e607a89c9c19
Denys Dovhan <email@denysdovhan.com>
2015-09-08 16:05:47 +0300

    Gulp: Fix for home page.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -74,9 +74,9 @@ gulp.task('index', ['collect'], () => {
       promises.push(new Promise((resolve, reject) => {
         render('layout/index.jade', {
           posts: onPage,
-          prevPage: page-1 !== 0                ? `page/${page-1}` : null,
-          nextPage: page*perPage < posts.length ? `page/${page+1}` : null
-        }, page != 1 ? `page/${page}` : '/')
+          prevPage: page-1 != 0 ? (page-1 == 1) ? '/':`/page/${page-1}` : false,
+          nextPage: page*perPage < posts.length ? `/page/${page+1}`     : false
+        }, page != 1 ? `/page/${page}` : '/')
           .on('error', reject)
           .on('end', resolve);
       }));
